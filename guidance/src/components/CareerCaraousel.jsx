be11d1db41e60e8143e64cf40d8f1cbf4c4a9731
Rochelle Katzman <zelrochelle@gmail.com>
2023-02-14 12:59:16 -0500

    carousel etc

--- guidance/src/components/CareerCaraousel.jsx
+++ guidance/src/components/CareerCaraousel.jsx
@@ -2,8 +2,213 @@ import React from 'react'
 
 function CareerCaraousel() {
   return (
-    <div>CareerCaraousel</div>
-  )
+    <div>
+      CareerCaraousel
+      <section className="bg-coolGray-50 py-4">
+        <div className="container px-4 mx-auto">
+          <div className="flex flex-wrap -m-3">
+            <div className="w-full md:w-1/3 p-3">
+              <div className="bg-white border border-coolGray-100 shadow-dashboard rounded-md">
+                <div className="flex flex-wrap items-center p-6 pb-4 -m-2 border-b border-coolGray-100 justify-center">
+                  <div className="w-full lg:w-auto p-2">
+                    <div className="flex flex-wrap items-center -m-2">
+                      <div className="w-auto p-2">
+                        <img
+                          src="flex-ui-assets/images/dashboard/cards/avatar.png"
+                          alt=""
+                        />
+                      </div>
+                      <div className="w-auto">
+                        <h2 className="text-sm font-medium text-coolGray-900">
+                          Coal Miner
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-auto p-2"></div>
+                </div>
+                <div className="flex flex-wrap px-4 py-6 border-b border-coolGray-100 -m-2">
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        $4k
+                      </p>
+                      <p className="text-xs text-coolGray-400 font-medium">
+                        Total Cost
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        1 Year
+                      </p>
+                      <p
+                        className="text-xs text-coolGray-400 font-medium"
+                        contenteditable="false"
+                      >
+                        Total Time
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        $1,000,000
+                      </p>
+                      <p className="text-xs text-coolGray-400 font-medium">
+                        Average Salary
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-wrap p-6 justify-center">
+                  <div className="w-full lg:w-auto p-2"></div>
+                  <div className="w-full lg:w-auto">
+                    <button className="flex flex-wrap justify-center w-full px-4 bg-green-500 hover:bg-green-600 font-medium text-sm text-white border border-green-500 rounded-md shadow-button py-2">
+                      <p>Add to Paths</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full md:w-1/3 p-3">
+              <div className="bg-white border border-coolGray-100 shadow-dashboard rounded-md">
+                <div className="flex flex-wrap items-center p-6 pb-4 -m-2 border-b border-coolGray-100 justify-center">
+                  <div className="w-full lg:w-auto p-2">
+                    <div className="flex flex-wrap items-center -m-2">
+                      <div className="w-auto p-2">
+                        <img
+                          src="flex-ui-assets/images/dashboard/cards/avatar.png"
+                          alt=""
+                        />
+                      </div>
+                      <div className="w-auto">
+                        <h2 className="text-sm font-medium text-coolGray-900">
+                          Gym Teacher
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-auto p-2"></div>
+                </div>
+                <div className="flex flex-wrap px-4 py-6 border-b border-coolGray-100 -m-2">
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        $24k
+                      </p>
+                      <p className="text-xs text-coolGray-400 font-medium">
+                        Total Cost
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        6 Years
+                      </p>
+                      <p
+                        className="text-xs text-coolGray-400 font-medium"
+                        contenteditable="false"
+                      >
+                        Total Time
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        $84k
+                      </p>
+                      <p className="text-xs text-coolGray-400 font-medium">
+                        Average Salary
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-wrap p-6 justify-center">
+                  <div className="w-full lg:w-auto p-2"></div>
+                  <div className="w-full lg:w-auto">
+                    <button className="flex flex-wrap justify-center w-full px-4 bg-green-500 hover:bg-green-600 font-medium text-sm text-white border border-green-500 rounded-md shadow-button py-2">
+                      <p>Add to Paths</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full md:w-1/3 p-3">
+              <div className="bg-white border border-coolGray-100 shadow-dashboard rounded-md">
+                <div className="flex flex-wrap items-center p-6 pb-4 -m-2 border-b border-coolGray-100 justify-center">
+                  <div className="w-full lg:w-auto p-2">
+                    <div className="flex flex-wrap items-center -m-2">
+                      <div className="w-auto p-2">
+                        <img
+                          src="flex-ui-assets/images/dashboard/cards/avatar.png"
+                          alt=""
+                        />
+                      </div>
+                      <div className="w-auto">
+                        <h2 className="text-sm font-medium text-coolGray-900">
+                          Nurse Anesthetist
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-auto p-2"></div>
+                </div>
+                <div className="flex flex-wrap px-4 py-6 border-b border-coolGray-100 -m-2">
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        $24k
+                      </p>
+                      <p className="text-xs text-coolGray-400 font-medium">
+                        Total Cost
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        6 Years
+                      </p>
+                      <p
+                        className="text-xs text-coolGray-400 font-medium"
+                        contenteditable="false"
+                      >
+                        Total Time
+                      </p>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 p-2 lg:w-1/3">
+                    <div className="text-center">
+                      <p className="mb-1 text-xs text-coolGray-900 font-semibold">
+                        $84k
+                      </p>
+                      <p className="text-xs text-coolGray-400 font-medium">
+                        Average Salary
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-wrap p-6 justify-center">
+                  <div className="w-full lg:w-auto p-2"></div>
+                  <div className="w-full lg:w-auto">
+                    <button className="flex flex-wrap justify-center w-full px-4 bg-green-500 hover:bg-green-600 font-medium text-sm text-white border border-green-500 rounded-md shadow-button py-2">
+                      <p>Add to Paths</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default CareerCaraousel
