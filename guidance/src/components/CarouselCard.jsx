bff27d939e06f029543b5e43a3c733c85f37929a
Veronique Cadet <cadetvero@gmail.com>
2023-05-23 00:52:48 -0400

    responsiveness for browse page

--- guidance/src/components/CarouselCard.jsx
+++ guidance/src/components/CarouselCard.jsx
@@ -8,27 +8,22 @@ function CarouselCard({ career, showsInfo, careerId }) {
   //   };
 
   return (
-    <div className="w-full p-3 md:w-1/3">
+    <div className="w-full p-3 lg:w-1/3">
       <div className="transition duration-200 bg-white border rounded-md border-coolGray-100 shadow-dashboard hover:bg-white hover:shadow-2xl">
-        <div className="flex flex-wrap items-center justify-center p-6 pb-4 -m-2 border-b border-coolGray-100">
-          <div className="w-full p-2 lg:w-auto">
+        <div className="flex flex-wrap items-center justify-center p-6 pb-4  border-b border-coolGray-100">
+          <div className="p-2 w-auto">
             <div className="flex flex-wrap items-center -m-2">
-              <div className="w-auto p-2">
-                <img
-                  src="flex-ui-assets/images/dashboard/cards/avatar.png"
-                  alt=""
-                />
-              </div>
+             
               <div className="w-auto">
-                <h2 className="text-xl font-medium text-green-800">
+                <h2 className="text-xl text-center font-medium text-green-800">
                   {career.name}
                 </h2>
               </div>
             </div>
           </div>
-          <div className="w-full p-2 lg:w-auto"></div>
+          
         </div>
-        <div className="flex flex-wrap px-4 py-6 -m-2 border-b border-coolGray-100">
+        <div className="flex flex-wrap px-4 py-6  border-b border-coolGray-100">
           <div className="w-full p-2 md:w-1/2 lg:w-1/3">
             <div className="text-center">
               <p className="mb-1 text-xs font-semibold text-green-500">
@@ -72,7 +67,7 @@ function CarouselCard({ career, showsInfo, careerId }) {
         <div className="flex flex-wrap justify-center p-6">
           <div className="w-full p-2 lg:w-auto"></div>
           <div className="w-full lg:w-auto">
-            <Link to="/careerdata" state={{from: career}}>
+            <Link to="/careerdata" state={{ from: career }}>
               <button
                 className="flex flex-wrap justify-center w-full px-4 py-2 text-sm font-medium text-white bg-green-500 border border-green-500 rounded-md hover:bg-green-600 shadow-button"
                 onClick={() => showsInfo(career.id)}
