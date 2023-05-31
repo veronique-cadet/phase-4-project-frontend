58b608e7a00883f7f8e6139381b25d414155192f
Rochelle Katzman <zelrochelle@gmail.com>
2023-05-31 16:57:02 -0400

    updated 404 page

--- guidance/src/components/NotFound404.jsx
+++ guidance/src/components/NotFound404.jsx
@@ -6,8 +6,18 @@ function NotFound404(props) {
     return (
       <div>
         <NavBarTwo />
-        OOps! The page you are looking for does not exist. 
-        <Footer/>
+        <div className="w-full px-4 ">
+          <div className="h-full p-8 text-center transition duration-200 rounded-md hover:bg-white hover:shadow-2xl">
+            <div className="inline-flex items-center justify-center w-16 h-16 mx-auto text-white bg-green-500 rounded-lg mb-7">
+              <img src="./money.png" />
+            </div>
+            <h3 className="mb-3 text-xl font-bold leading-tight md:text-2xl">
+              404 Not Found: Oops! The page you are looking for does not exist
+            </h3>
+            
+          </div>
+        </div>
+        <Footer />
       </div>
     );
 }
