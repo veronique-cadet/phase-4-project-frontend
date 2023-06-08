1c819cd785908ebecc1f7827367460587912e339
Veronique Cadet <cadetvero@gmail.com>
2023-06-08 15:41:18 -0400

    adding height to your paths page

--- guidance/src/components/YourPaths.jsx
+++ guidance/src/components/YourPaths.jsx
@@ -50,7 +50,7 @@ function YourPaths({ yourPaths, setYourPaths, user, setUser }) {
     <div>
       <NavBarTwo setUser={setUser} />
       {user ? (
-        <div>
+        <div className="h-screen">
           <h2 className="pb-5 mt-5 mb-4 text-4xl font-bold tracking-tighter text-center md:text-5xl text-coolGray-900">
             Your Paths&nbsp;
           </h2>
@@ -69,7 +69,7 @@ function YourPaths({ yourPaths, setYourPaths, user, setUser }) {
           {/* {pathCard} */}
         </div>
       ) : (
-        <div>
+        <div className="h-screen">
           <h2 className="pb-5 mt-5 mb-4 text-4xl font-bold tracking-tighter text-center md:text-5xl text-coolGray-900">
             Your Paths&nbsp;
           </h2>
